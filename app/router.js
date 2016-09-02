d16ed318848c63004ae0a0dad57a7bfa750eb777
Elana Kopelevich <ekopelevich@gmail.com>
2016-09-02 16:03:10 -0600

    halfway through tutorial - working on filter component

--- app/router.js
+++ app/router.js
@@ -1,12 +1,13 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from 'ember'
+import config from './config/environment'
 
 const Router = Ember.Router.extend({
-  location: config.locationType
-});
+  location: config.locationType,
+})
 
 Router.map(function() {
-  this.route('about');
-});
+  this.route('about')
+  this.route('contact')
+})
 
-export default Router;
+export default Router
